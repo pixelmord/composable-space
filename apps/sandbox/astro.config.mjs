d5909012b2628583c0bf56407a48d4a798618ece
Andreas Adam <andreas.adam@parcellab.com>
2024-08-20 16:23:06 +0200

    chore: install tailwind

--- apps/sandbox/astro.config.mjs
+++ apps/sandbox/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sentry(), spotlightjs()]
+});
